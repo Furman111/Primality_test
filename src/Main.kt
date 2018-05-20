c1649270e7bd6cee9477a762e4ab5ad1d73b770a
furmansd <furmansd@altarix.ru>
2018-05-20 15:43:16 +0400

    Готово

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,17 @@
+import java.math.BigInteger
+
+const val BITS_NUMBER = 512
+
 fun main(args: Array<String>) {
-    var x = generate(63)
-    while(!checkDivisibilityWithPrimitives(x)){
-        x = generate(63)
+    var x = generate(BITS_NUMBER)
+
+    var isPrimitive = testMillerRabin(x)
+
+    while (!isPrimitive) {
+        x = x.add(BigInteger.valueOf(2))
+
+        isPrimitive = testMillerRabin(x)
     }
+
 }
+
